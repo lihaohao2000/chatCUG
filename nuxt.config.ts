3cec4158ddb451fae605dd15755d7ef0a5ea438d
Li Yonghao <80660014+lihaohao2000@users.noreply.github.com>
2024-03-26 14:19:28 +0800

    Use script to change port to 80.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  server: {
-    port: 80
-  },
   devtools: { enabled: true },
   modules: [
     '@nuxt/ui',
