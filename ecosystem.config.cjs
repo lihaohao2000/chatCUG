022c7fe2e4d4d430635f547343f83c84f68b0320
Li Yonghao <80660014+lihaohao2000@users.noreply.github.com>
2024-04-05 22:14:47 +0800

    v0.0.1

---
+++ ecosystem.config.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+    apps: [
+      {
+        name: 'ChatCUG',
+        exec_mode: 'cluster',
+        instances: 'max',
+        script: './server/index.mjs',
+        env: {
+            NODE_ENV: 'production',
+            PORT: 80,
+            DISABLE_VERCEL_ANALYTICS: false,
+            DATABASE_URL: 'file: ../ChatCUG.sqlite',
+            CHROMADB_URL: 'http://localhost:8000',
+            LANGCHAIN_TRACING_V2: false,
+            LANGCHAIN_PROJECT: 'chat-ollama'
+        }
+      }
+    ]
+  };
+  
