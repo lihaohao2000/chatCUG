42b2a81d27019521bed644dbd725a84399ccaf7e
Li Yonghao <80660014+lihaohao2000@users.noreply.github.com>
2024-04-06 11:45:21 +0800

    v0.0.3

--- app.vue
+++ app.vue
@@ -1,26 +1,8 @@
 <script setup lang="ts">
-  import { useStorage } from '@vueuse/core';
-  import prisma from '@/server/utils/prisma';
   useSeoMeta({
     description: 'ChatCUG是中国联通国际公司内部使用的定制化语言模型平台。'
   })
-  const globalModel = useStorage(`model`, process.env.DEFAULT_MODEL);
-  const globalEmbed = useStorage(`embed`, process.env.DEFAULT_EMBED);
-  console.log(globalModel)
-  console.log(globalEmbed)
-  console.log(process.env.DATABASE_URL)
-  const defaultKnowledgeBaseName = "DEFAULT_KNOWLEDGEBASE"
-  if (await prisma.knowledgeBase.count({}) == 0) {
-      const affected = await prisma.knowledgeBase.create({
-          data: {
-            name: defaultKnowledgeBaseName,
-            description: "The default knowledgebase, all files will be added in this base.",
-            embedding: globalEmbed.value,
-            created: new Date()
-          }
-        });
-        console.log(`Initialized knowledge base ${affected.name}, ID: ${affected.id}, Embedding model: ${globalEmbed.value}.`);
-  }
+  await $fetch(`/api/init/`);
 </script>
 <template>
   <div class="h-full">
